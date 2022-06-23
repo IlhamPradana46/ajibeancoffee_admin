087e6df54b2bfbe65d84cf820dfa364aaa15f893
hanthoru <thoriqfirdauss@gmail.com>
2022-06-23 10:43:19 +0700

    Fixing navbar not collapsing in small iew port

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo base_url().'asset/css/bootstrap.min.css' ?>">
   <link rel="stylesheet" href="<?php echo base_url().'asset/css/index.css' ?>">
@@ -12,19 +11,19 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/913d7811ef.js" crossorigin="anonymous"></script>
 
-<title>AJIBean Coffee</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid text-center">
-    <div class="container">
+  <title>AJIBean Coffee</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid text-center">
+      <div class="container">
+      </div>
     </div>
-  </div>
 
     <nav class="navbar navbar-expand-lg bg-brown">
       <div class="container">
-      <img src="<?php echo base_url().'asset/image/image01.png'; ?>" alt="" style="width:80px;height:60px;"><a class="navbar-brand text-white"><strong>AJIBean</strong> Coffee</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon text-white"></span>
+      <a class="navbar-brand text-white"><img src="<?php echo base_url().'asset/image/image01.png'; ?>" alt="" style="width:80px;height:60px;"><strong>AJIBean</strong> Coffee</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fa-solid fa-bars" style="color:white;"></i>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarNav">
@@ -46,3 +45,4 @@
      </div> 
     </nav>
 </body>
+</html>
