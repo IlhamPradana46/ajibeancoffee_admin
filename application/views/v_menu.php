5712116b829b24b61761a6be0d567cf27df4b775
IlhamPradana46 <m.ilhamegip@gmail.com>
2022-06-25 23:52:11 +0700

    fixing bug edit data dengan modal

--- application/views/v_menu.php
+++ application/views/v_menu.php
@@ -1,9 +1,9 @@
 <body>
 <div class="container" style="">
     <div class="container">
-    <button type="button" class="btn btn-success mt-3" data-toggle="modal" data-target="#exampleModal">
-    TAMBAH DAFTAR MENU
-    </button>
+        <button type="button" class="btn btn-success mt-3" data-toggle="modal" data-target="#exampleModal">
+        TAMBAH DAFTAR MENU
+        </button>
 
     <!-- Modal Tambah -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -33,7 +33,7 @@
                         <label for="gambar">Gambar</label>
                         <input name="gambar" type="file" class="form-control-file" id="file">
                     </div>
-                    </div>
+            </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">CLOSE</button>
                         <input class="btn btn-primary btn-outline-light" type="submit" value="SIMPAN">
@@ -49,18 +49,21 @@
         <div class="col-md-3 mt-3">
             <div class="card border-dark">
               <img src="<?php echo base_url().'upload/images/'.$p->gambar ?>" class="card-img-top" alt="...">
-              <div class="card-body">
-               <h5 class="card-title font-weight-bold"><?php echo $p->nama_produk ?></h5>
-               <label class="card-text harga">Stok <?php echo $p->stok ?></label><br>
-               <label class="card-text harga">Rp. <?php echo number_format($p->harga, 2, ",", ".");?></label><br>
-               <button type="button" data-toggle="modal" data-target="#modalEdit" class="btn btn-success btn-outline-dark text-white">EDIT</button> <a href="<?php echo base_url().'produk/delete_produk/'.$p->id_produk; ?>" class="btn btn-danger btn-outline-dark text-white">DELETE</a>
-              </div>
+                <div class="card-body">
+                    <h5 class="card-title font-weight-bold"><?php echo $p->nama_produk ?></h5>
+                    <label class="card-text harga">Stok <?php echo $p->stok ?></label><br>
+                    <label class="card-text harga">Rp. <?php echo number_format($p->harga, 2, ",", ".");?></label><br>
+                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalEdit<?= $p->id_produk ?>">Edit </button>
+                    <a href="<?php echo base_url().'produk/delete_produk/'.$p->id_produk; ?>" class="btn btn-danger btn-outline-dark text-white">DELETE</a>
+                </div>
             </div>
         </div>
     <?php } ?>
     </div>
         <!-- Modal Ubah -->
-        <div class="modal fade" id="modalEdit" tabindex="-1" aria-labelledby="modalEditLabel" aria-hidden="true">
+        <?php $no = 0 ?>
+        <?php foreach ($produk as $p) : $no++ ?>
+        <div class="modal fade" id="modalEdit<?= $p->id_produk; ?>" tabindex="-1" aria-labelledby="modalEditLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -70,40 +73,41 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        <?php foreach($produk as $p){	?>
+                        <!-- <?php //foreach($produk as $p){	?> -->
                         <?php echo form_open_multipart(base_url().'produk/update_produk'); ?>
-                            <input type="hidden" name="id_produk" value="<?=$p->id_produk?>">
+                            <input type="hidden" name="id_produk" id="id_produk" value="<?= $p->id_produk ?>">
                             <div class="form-group">
                                 <label for="nama">Nama Produk</label>
-                                <input name="nama" type="text" class="form-control" id="nama" placeholder="Input nama produk" value="<?=$p->nama_produk?>">
-                            </div>
+                                <input name="nama" type="text" class="form-control" id="nama" placeholder="Input nama produk" value="<?= $p->nama_produk ?>" >
+                            </div>  
                             <div class="form-group">
                                 <label for="stok">Stok</label>
-                                <input name="stok" type="number" class="form-control" id="stok" value="<?=$p->stok?>">
+                                <input name="stok" type="number" class="form-control" id="stok" value="<?= $p->stok ?>">
                             </div>
                             <div class="form-group">
                                 <label for="harga">Harga</label>
-                                <input name="harga" type="number" class="form-control" id="harga" value="<?=$p->harga?>">
+                                <input name="harga" type="number" class="form-control" id="harga" value="<?= $p->harga ?>">
                             </div>
                             <div class="form-group">
                                 <label for="gambar">Gambar</label>
                                 <input name="gambarbaru" type="file" class="form-control-file" id="file">
                             </div>
                             <!-- file lama -->
-                            <input type="hidden" name="gambarlama" value="<?=$p->gambar?>">
+                            <input type="hidden" name="gambarlama" value="<?= $p->gambar ?>">
                             </div>
-                            <!-- <div class="modal-footer">
+                            <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">CLOSE</button>
                                 <input class="btn btn-primary btn-outline-light" type="submit" value="SIMPAN">
-                            </div> -->
+                            </div>
                         <?php echo form_close() ?>
-                    <?php } ?>
+                    <!-- <?php // } ?> -->
                 </div>
             </div>
         </div>
+        <?php endforeach ?>
         <!-- End Modal Ubah -->
     </div>
-    </div>
+</div>
 <br>
 <br>
 <br>
